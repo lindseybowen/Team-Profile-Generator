1aa4afd3f2b5a97a688689e6f4f90d603ec88860
lindsey Bowen <lindseyjbowen@gmail.com>
2021-01-22 19:53:33 -0500

    added in engineer and intern

--- index.js
+++ index.js
@@ -35,4 +35,82 @@ function managerPrompt(){
         teamMemPrompt();
     })
 }
+function engineerPrompt(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: 'What is the name of the engineer you are inputting?',
+        },
+        {
+            type: 'input',
+            name: 'engineerID',
+            message: 'What is the employee ID for the engineer?',
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: 'What is the engineers email?',
+        },
+        {
+            type: 'input',
+            name: 'engineerGithub',
+            message: 'What is the Github username of the engineer?',
+        },
+    ]).then(function(response){
+        var engineerName = response.engineerName;
+        var engineerID = response.engineerID;
+        var engineerEmail = response.engineerEmail;
+        var engineerGithub = response.engineerNumber
+        const newTeamProfile = new Engineer(engineerName, engineerID, engineerEmail, engineerGithub);
+        finalTeam.push(newTeamProfile);
+        teamMemPrompt();
+    })
+}    
+
+function internPrompt(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: 'What is the name of the intern you are inputting?',
+        },
+        {
+            type: 'input',
+            name: 'internID',
+            message: 'What is the employee ID for the intern?',
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: 'What is the interns email?',
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: 'What school does the intern go to?',
+        },
+    ]).then(function(response){
+        var internName = response.internName;
+        var internID = response.internID;
+        var internEmail = response.internEmail;
+        var internSchool = response.internSchool
+        const newTeamProfile = new Intern(internName, internID, internEmail, internSchool);
+        finalTeam.push(newTeamProfile);
+        teamMemPrompt();
+    })
+} 
+    
+    function teamMemPrompt(){
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'addMore',
+                message: 'Would you like to add another member to your team?',
+                choices: ['Add another Engineer.', 'Add another Intern.', 'No, my team is complete.']
+            },
+        ]);
+    }
+
+
 
